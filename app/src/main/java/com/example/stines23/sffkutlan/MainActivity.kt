f5e5868b0c6c884a3fdadeb25aeb16cd4202b62c
Nesdal <stian.nesdal@gmail.com>
2018-12-13 10:52:52 +0100

    Lagt inn webview

--- app/src/main/java/com/example/stines23/sffkutlan/MainActivity.kt
+++ app/src/main/java/com/example/stines23/sffkutlan/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.stines23.sffkutlan
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
-
+    var mywebview: WebView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mywebview = findViewById<WebView>(R.id.webview)
+        mywebview!!.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        mywebview!!.loadUrl("https://www.google.co.in/")
     }
 }
